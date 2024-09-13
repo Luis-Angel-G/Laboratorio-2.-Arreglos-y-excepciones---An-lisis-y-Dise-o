1459180ecf7f792f22a7d79844dfce64b4c73404
Luis-Angel-G <langelgirona@gmail.com>
2024-09-12 21:49:52 -0600

    Se corrige Secciones.java

--- Secciones.java
+++ Secciones.java
@@ -1,6 +1,41 @@
 public class Secciones {
-	private int[][] vip = new int[5][10];
-	private int[][] platino = new int[8][12];
-	private int[][] oro = new int[10][15];
-	private int[][] plata = new int[12][20];
+    private int[][] vip = new int[5][10];
+    private int[][] platino = new int[8][12];
+    private int[][] oro = new int[10][15];
+    private int[][] plata = new int[12][20];
+
+    public Secciones() {
+        inicializarSecciones();
+    }
+
+    private void inicializarSecciones() {
+        for (int i = 0; i < vip.length; i++)
+            for (int j = 0; j < vip[i].length; j++)
+                vip[i][j] = 1;
+
+        for (int i = 0; i < platino.length; i++)
+            for (int j = 0; j < platino[i].length; j++)
+                platino[i][j] = 1;
+
+        for (int i = 0; i < oro.length; i++)
+            for (int j = 0; j < oro[i].length; j++)
+                oro[i][j] = 1;
+
+        for (int i = 0; i < plata.length; i++)
+            for (int j = 0; j < plata[i].length; j++)
+                plata[i][j] = 1;
+    }
+
+    public int[][] getVip() {
+		return vip;
+	}
+    public int[][] getPlatino() {
+		return platino;
+	}
+    public int[][] getOro() {
+		return oro;
+	}
+    public int[][] getPlata() {
+		return plata;
+	}
 }
