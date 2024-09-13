36c2d75a6b541483bcb184722fe83fc0fddd046e
Luis-Angel-G <langelgirona@gmail.com>
2024-09-13 11:19:18 -0600

    Se quitan tildes

--- DriverProgram.java
+++ DriverProgram.java
@@ -12,41 +12,41 @@ public class DriverProgram {
             System.out.println("3. Cancelar Reserva");
             System.out.println("4. Calcular Ingresos");
             System.out.println("5. Salir");
-            System.out.print("Seleccione una opción: ");
+            System.out.print("Seleccione una opcion: ");
             opcion = teclado.nextInt();
 
             switch (opcion) {
                 case 1:
-                    System.out.print("Ingrese sección (1: VIP, 2: Platino, 3: Oro, 4: Plata): ");
+                    System.out.print("Ingrese seccion (1: VIP, 2: Platino, 3: Oro, 4: Plata): ");
                     int seccion = teclado.nextInt();
                     System.out.print("Ingrese fila: ");
                     int fila = teclado.nextInt();
                     System.out.print("Ingrese asiento: ");
                     int asiento = teclado.nextInt();
-                    gestion.reservarAsientos(seccion, fila, asiento);
+                    System.out.print(gestion.reservarAsientos(seccion, fila, asiento));
                     break;
                 case 2:
-                    System.out.print("Ingrese sección para mostrar disponibilidad (1: VIP, 2: Platino, 3: Oro, 4: Plata): ");
+                    System.out.print("Ingrese seccion para mostrar disponibilidad (1: VIP, 2: Platino, 3: Oro, 4: Plata): ");
                     seccion = teclado.nextInt();
-                    gestion.mostrarDisponibilidad(seccion);
+                    System.out.print(gestion.mostrarDisponibilidad(seccion));
                     break;
                 case 3:
-                    System.out.print("Ingrese sección (1: VIP, 2: Platino, 3: Oro, 4: Plata): ");
+                    System.out.print("Ingrese seccion (1: VIP, 2: Platino, 3: Oro, 4: Plata): ");
                     seccion = teclado.nextInt();
                     System.out.print("Ingrese fila: ");
                     fila = teclado.nextInt();
                     System.out.print("Ingrese asiento: ");
                     asiento = teclado.nextInt();
-                    gestion.cancelarReserva(seccion, fila, asiento);
+                    System.out.print(gestion.cancelarReserva(seccion, fila, asiento));
                     break;
                 case 4:
-                    gestion.calcularIngresos();
+                    System.out.print(gestion.calcularIngresos());
                     break;
                 case 5:
                     System.out.println("Saliendo...");
                     break;
                 default:
-                    System.out.println("Opción no válida.");
+                    System.out.println("Opcion no valida.");
                     break;
             }
         } while (opcion != 5);
