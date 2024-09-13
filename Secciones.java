fbeb118a002a6c9e2370f5dc8f035c11bbeeba75
Luis-Angel-G <langelgirona@gmail.com>
2024-09-12 19:18:06 -0600

    Se deja solo los atributos de secciones

--- Secciones.java
+++ Secciones.java
@@ -3,15 +3,4 @@ public class Secciones {
 	private int[][] platino = new int[8][12];
 	private int[][] oro = new int[10][15];
 	private int[][] plata = new int[12][20];
-	
-	public Secciones() {
-		
-	}
-	
-	public Secciones(int[][] vip, int[][] platino, int[][] oro, int[][] plata) {
-		this.vip = vip;
-		this.platino = platino;
-		this.oro = oro;
-		this.plata = plata;
-	}
 }
